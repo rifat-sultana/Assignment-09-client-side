7c68139273ba9ccf1629c7893efa8070c6262335
rifat-sultana <rifatsultana88.web@gmail.com>
2026-05-23 00:57:18 +0600

    fix client env variable

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: `${process.env.API_KEY}`,
+  baseURL: process.env.NEXT_PUBLIC_API_KEY,
   fetchOptions: {
     credentials: "include",
   },
