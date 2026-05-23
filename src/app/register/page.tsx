98eff3e43fb5a0b33994cbfb527ff4f3907841cc
rifat-sultana <rifatsultana88.web@gmail.com>
2026-05-23 22:57:26 +0600

    Refactor import paths to use absolute imports and add "use client" directive

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { useState } from "react";
-import { authClient } from "../lib/auth-client";
+import { authClient } from "@/lib/auth-client";
 
 const Register = () => {
   const [name, setName] = useState("");
